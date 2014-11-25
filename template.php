d2493a3c61a5633fae6fd75469f1ac36b70e6d3f
leihro <leiwspe@gmail.com>
2014-11-25 15:08:24 +0100

    update template

--- template.php
+++ template.php
@@ -8,49 +8,18 @@
     <link rel="stylesheet" type="text/css" href="_/css/custom.css">
 </head>
 
-<body id = "home">
+<body>
 
 <!-- row 1: navigation -->
-    <nav class = "navbar navbar-inverse navbar-static-top" role = "navigation">
-        <div class = "container">
-            <!-- mobile display -->
-            <div class = "navbar-header">
-                <button type = "button" class = "navbar-toggle collapsed" data-toggle = "collapse" data-target = "#navbar_collapse_1">
-                    <span class = "sr-only">Toggle navigation</span>
-                    <span class = "icon-bar"></span>
-                    <span class = "icon-bar"></span>
-                    <span class = "icon-bar"></span>
-                </button>
-                <a class = "navbar-brand" href="#">DOTA2</a>
-            </div>
-
-            <!-- collect everything from toggling -->
-            <div class = "collapse navbar-collapse" id = "navbar_collapse_1">
-                <ul class = "nav navbar-nav">
-                    <li class = "active"><a href="#">Home<span class = "sr-only">(current)</span></a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-                <form class = "navbar-form navbar-right" role = "search">
-                    <div class = "form-group">
-                        <input type = "text" class = "form-control" placeholder = "Search">
-                    </div>
-                    <button type = "submit" class = "btn btn-sm btn-default">Search</button>
-                </form>
-            </div>  <!-- navbar collapse -->
-        </div>  <!-- navbar container -->
-    </nav>
-
+    <?php include("_/components/php/nav.php") ?>
 
 <section class="container">
 
-    
 <!-- row 2: header -->
 
     <header class = "row">
 
     </header>
-
 
 <!-- row 3: article/aside -->
 
@@ -58,19 +27,8 @@
         <p>hello world</p>
     </div>
 
-
 <!-- row 4: footer -->
-
-    <footer class = "row">
-        <div class = "col-lg-8 col-lg-offset-4">
-            <ul class = "breadcrumb">
-                <li><a href="#">Site map</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Terms of Service</a></li>
-                <li><a href="#">Website use policy</a></li>
-            </ul>
-        </div>
-    </footer>
+    <?php include("_/components/php/footer.php") ?>
 
 
 </section> <!-- end container -->
@@ -79,8 +37,8 @@
 <!-- javascript -->
 
 
-	<script type="text/javascript" src = "_/js/jquery.min.js"></script>
-	<script type="text/javascript" src = "_/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src = "_/js/jquery.min.js"></script>
+    <script type="text/javascript" src = "_/js/bootstrap.min.js"></script>
     <script type="text/javascript" src = "_/js/custom.js"></script>
 </body>
 </html>
